0a74b921e764a43eeb49f6daf862cbb8dad405e8
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-06-03 14:34:37 -0300

    feat: add google images search

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const robots = {
     input: require('./robots/input.js'),
     text: require('./robots/text.js'),
-    state: require('./robots/state.js')
+    state: require('./robots/state.js'),
+    image: require('./robots/image.js')
 }
 
 async function start() {
 
-    robots.input()
+    // robots.input()
 
-    await robots.text()
+    // await robots.text()
+
+    await robots.image()
 
     const content = robots.state.load()
 
